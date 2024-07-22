9a3e113676e87882183c1b22e7a4a7d4eb7be99a
Nich Brockman <nbrockman19@gmail.com>
2024-07-21 19:04:20 -0500

    setup adding and clearing cards to hand or cut

--- src/components/content/HandCounter/handCounter.tsx
+++ src/components/content/HandCounter/handCounter.tsx
@@ -1,17 +1,48 @@
 import { useContext, useEffect, useState } from 'react';
 import {
   Button,
-  Input
+  Col,
+  Input,
+  Radio,
+  RadioChangeEvent,
+  Row
 } from 'antd';
 import classNames from 'classnames';
 import { BaseContext } from '../../base/Base';
 import './handCounter.css';
-import { FullDeckCards } from '../../../types/card';
+import { Card, FullDeckCards } from '../../../types/card';
 
 export const StartPage = () => {
   const baseContext = useContext(BaseContext);
   useEffect(() => baseContext.setTitle('Calculate Hand Score'));
+  const [isAddingToHand, setIsAddingToHand] = useState(true);
+  const [handCards, setHandCards] = useState<Card[] | null>([]);
+  const [cutCards, setCutCards] = useState<Card[] |  null>([]);
   const [handScore, setHandScore] = useState<number | null>(null);
+
+  const handleCardLocationChange = (e: RadioChangeEvent) => {
+    setIsAddingToHand(e.target.value);
+  }
+
+  const handleCardClick = (selectedCard: Card) => {
+    if (isAddingToHand){
+      setHandCards([...handCards, selectedCard]);
+    }
+    else 
+    {
+      setCutCards([...cutCards, selectedCard])
+    }
+  }
+
+  const handleClearSelection = () => {
+    if (isAddingToHand){
+      setHandCards([]);
+    }
+    else 
+    {
+      setCutCards([])
+    }
+  }
 
   const handleCalculateScore = () => {
     const inputString = (document.getElementById("cardInput") as HTMLInputElement).value.trim();
@@ -21,10 +52,6 @@ export const StartPage = () => {
       alert("Please enter cards in valid format. Must include a colon to separate hand and cut cards")
       return;
     }
-    const handAndCutCards = inputString.split(":");
-    const handCards = handAndCutCards[0];
-    const cutCards = handAndCutCards[1];
-
     const deckOfCards = FullDeckCards;
     alert(deckOfCards);
 
@@ -34,34 +61,63 @@ export const StartPage = () => {
   return (
     <div>
       <div className="margin-bottom-md">
-        Type the cards in your hand and the cut card(s) into the box below. <br/>
-        Cards must be input in format of <strong>&#123;rank&#125;&#123;suit&#125;</strong>, with a colon separating the cards in your hand and the cut cards. <br/><br/>
-        For example, if your hand is:
-        <div>
-          <li>Ace of Spades</li>
-          <li>Four of Diamonds</li>
-          <li>Five of Clubs</li>
-          <li>Jack of Hearts</li>
+         First select where you are adding your cards too (hand or cut cards), and then click on a card to add it that location.
+      </div>
+      <Row>
+        <Col xs={2} sm={4} md={6}>
+          <Row className="margin-bottom-md">
+            <Col span={8}>
+            <Radio.Group 
+              onChange={handleCardLocationChange} 
+              value={isAddingToHand} 
+              optionType="button"
+              buttonStyle="solid"
+              >
+              <Radio value={true}>Hand</Radio>
+              <Radio value={false}>Cut</Radio>
+            </Radio.Group>
+            </Col>
+            <Col>
+              <Button onClick={handleClearSelection}>Clear selection</Button>
+            </Col>
+          </Row>
+          <div className="margin-bottom-md">
+            Click on a card to add to {isAddingToHand ? "your hand." : "the cut."}
+          </div>
+        </Col>
+        <Col span={8}>
+        <div className="margin-bottom-md">
+            <Button type="primary" onClick={handleCalculateScore}>Click to count hand</Button>
+          </div>
+          <Row className="margin-bottom-md">
+            <Col xs={8}>
+              Hand Cards
+            </Col>
+            <Col xs={8}>
+              Cut Cards
+            </Col>
+          </Row>
+          <Row className="margin-bottom-md">
+            <Col xs={8}>
+              {handCards.map((handCard) => (
+                <div>{handCard.value}</div>
+              ))}
+            </Col>
+            <Col xs={8}>
+            {cutCards.map((cutCard) => (
+                <div>{cutCard.value}</div>
+              ))}
+            </Col>
+          </Row>
+          
+        </Col>
+      <br/>
+      {handScore && (
+        <div className="margin-bottom-md">
+          Your total hand score is: <strong>{handScore}</strong>
         </div>
-        and there are two cards that were cut:
-        <div>
-          <li>Eight of Spades</li>
-          <li>Two of Hearts</li>
-        </div>
-        the input would look like: "<strong>AS 4D 5C JH : 8S 2H</strong>"<br/>
-      </div>
-      <div className="margin-bottom-md">
-        <Input id="cardInput" placeholder='Input cards (i.e. 3C 4D 5S JH : 10H)' />
-      </div>
-      <div className="margin-bottom-md">
-        <Button type="primary" onClick={handleCalculateScore}>Click to count hand</Button>
-      </div>
-     <br/>
-     {handScore && (
-       <div className="margin-bottom-md">
-        Your total hand score is: <strong>{handScore}</strong>
-      </div>
-      )}
+        )}
+      </Row>
     </div>
   );
 }
