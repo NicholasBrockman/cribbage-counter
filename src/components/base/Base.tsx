507abdd123e1a1d2cec93581dde503580ce74bc8
Nich Brockman <nbrockman19@gmail.com>
2024-07-24 19:37:40 -0500

    added navbar collapse back

--- src/components/base/Base.tsx
+++ src/components/base/Base.tsx
@@ -41,7 +41,7 @@ export const Base = ({
   return (
     <BaseContext.Provider value={{setTitle: setBaseTitle, title: baseTitle}}>
       <Layout hasSider>
-        <Layout.Sider theme='light'>
+        <Layout.Sider theme='light' collapsed={navBarCollapsed} onCollapse={setNavBarCollapsed} onBreakpoint={setNavBarCollapsed} breakpoint='md'>
           <SideBar />
         </Layout.Sider>
         <Layout>
